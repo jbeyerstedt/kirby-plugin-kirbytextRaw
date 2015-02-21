5a5146e41d5d79123a67694e4757439b94e0be8b
Jannik Beyerstedt <jtByt.Pictures@gmail.com>
2015-02-21 16:57:23 +0100

    new email address

--- kirbytextRaw.php
+++ kirbytextRaw.php
@@ -3,7 +3,7 @@
  * kirby 2 plugin - kirbytextRaw
  * parse text as kirbytext but remove the p tags
  *
- * copyright: Jannik Beyerstedt | http://jannikbeyerstedt.de | jtByt.Pictures@gmail.com
+ * copyright: Jannik Beyerstedt | http://jannikbeyerstedt.de | code@jannikbeyerstedt.de
  * license: http://www.gnu.org/licenses/gpl-3.0.txt GPLv3 License
  * v1.0.1 (27.11.2014)
  * 
